09d6f516b256d2dd12efe3e9a5cd53376c5491f9
lrog <lucas.roguski@gmail.com>
2019-10-07 12:58:29 +0100

    fixing the test script, removing the medcat version requirement

--- models/download_medmen.sh
+++ models/download_medmen.sh
@@ -2,11 +2,15 @@
 set -e
 
 # output model files
-MODEL_CDB=./medmen/cdb.dat
-MODEL_VCB=./medmen/vocab.dat
+MEDMEN_DIR=./medmen
+MODEL_CDB=$MEDMEN_DIR/cdb.dat
+MODEL_VCB=$MEDMEN_DIR/vocab.dat
 
 if [[ ! -f "$MODEL_CDB"  || ! -f "$MODEL_VCB" ]]; then
   echo "Downloading models: MedMentions"
+  if [[ ! -d $MEDMEN_DIR ]]; then
+    mkdir $MEDMEN_DIR
+  fi
   # download the models as described in the MedCAT repo
   curl https://s3-eu-west-1.amazonaws.com/zkcl/vocab.dat > $MODEL_VCB
   curl https://s3-eu-west-1.amazonaws.com/zkcl/cdb-medmen-clean.dat > $MODEL_CDB
