8d4134a897699070bdbe8b5056775db20167f642
lrog <lucas.roguski@gmail.com>
2019-10-07 13:07:45 +0100

    updated link for downloading medmen model

--- models/download_medmen.sh
+++ models/download_medmen.sh
@@ -13,7 +13,7 @@ if [[ ! -f "$MODEL_CDB"  || ! -f "$MODEL_VCB" ]]; then
   fi
   # download the models as described in the MedCAT repo
   curl https://s3-eu-west-1.amazonaws.com/zkcl/vocab.dat > $MODEL_VCB
-  curl https://s3-eu-west-1.amazonaws.com/zkcl/cdb-medmen-clean.dat > $MODEL_CDB
+  curl https://s3-eu-west-1.amazonaws.com/zkcl/cdb-medmen.dat > $MODEL_CDB
 else
   echo "MedMentions models are present -- skipping downloading"
 fi
