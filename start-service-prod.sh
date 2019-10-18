f3e1992e12f5b2796b43bfff599ec1e8931bb889
lrog <lucas.roguski@gmail.com>
2019-10-18 12:48:24 +0100

    added gunicorn worker timeout parameter; updated medcat env params

--- start-service-prod.sh
+++ start-service-prod.sh
@@ -3,22 +3,27 @@
 # check the gunicorn config params
 #
 if [ -z ${SERVER_HOST+x} ]; then
-  echo "SERVER_HOST is unset -- setting to default: 0.0.0.0";
   SERVER_HOST=0.0.0.0;
+  echo "SERVER_HOST is unset -- setting to default: $SERVER_HOST";
 fi
 
 if [ -z ${SERVER_PORT+x} ]; then
-  echo "SERVER_PORT is unset -- setting to default: 5000";
   SERVER_PORT=5000;
+  echo "SERVER_PORT is unset -- setting to default: $SERVER_PORT";
 fi
 
 if [ -z ${SERVER_WORKERS+x} ]; then
-  echo "SERVER_WORKERS is unset -- setting to default: 1";
   SERVER_WORKERS=1;
+  echo "SERVER_WORKERS is unset -- setting to default: $SERVER_WORKERS";
+fi
+
+if [ -z ${SERVER_WORKER_TIMEOUT+x} ]; then
+  SERVER_WORKER_TIMEOUT=300;
+  echo "SERVER_WORKER_TIMEOUT is unset -- setting to default (sec): $SERVER_WORKER_TIMEOUT";
 fi
 
 
 # start the server
 #
 echo "Starting up Flask app using gunicorn server ..."
-gunicorn --bind $SERVER_HOST:$SERVER_PORT --workers=$SERVER_WORKERS --log-file=- --log-level info wsgi
+gunicorn --bind $SERVER_HOST:$SERVER_PORT --workers=$SERVER_WORKERS --timeout=$SERVER_WORKER_TIMEOUT --log-file=- --log-level info wsgi
