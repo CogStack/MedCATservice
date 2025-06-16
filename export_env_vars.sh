c279f00a666369b779edc8ac4381ee40165d5292
vladd-bit <vlad.a.dinu@gmail.com>
2025-06-16 11:16:54 +0100

    Env var + script updates.

--- export_env_vars.sh
+++ export_env_vars.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-set -o allexport
+set -a
 
 current_dir=$(pwd)
-security_dir="../security/"
 
 env_files=("env/general.env"
            "env/app.env"
            "env/medcat.env"
            )
 
-set -a
 
 for env_file in ${env_files[@]}; do
   source $env_file
 done
 
-# for nginx vars
-export DOLLAR="$"
-
 set +a
-
-set +o allexport
